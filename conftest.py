385157ac97b84c2376dc33081c6791b23b10e442
closethereport <closethereport@gmail.com>
2026-03-14 22:26:10 +0300

    Move screenshot fixture to ui_tests only

--- conftest.py
+++ conftest.py
@@ -5,15 +5,6 @@ import os
 load_dotenv()
 
 
-@pytest.fixture(autouse=True)
-def screenshot_on_failure(request, page):
-    yield
-    if request.node.rep_call.failed:
-        os.makedirs("reports/screenshots", exist_ok=True)
-        screenshot_path = f"reports/screenshots/{request.node.nodeid.replace('/', '_').replace('::', '_')}.png"
-        page.screenshot(path=screenshot_path)
-
-
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     outcome = yield
